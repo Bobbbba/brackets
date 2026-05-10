99d8cef3d1ff15ba510bdae9d770c2651b2058ef
Bobbbba <zwlad@tut.by>
2026-05-11 01:42:07 +0300

    feat: 28 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const pairs = Object.fromEntries(bracketsConfig);
+  const openBrackets = new Set(Object.keys(pairs));
+
+  const resultStack = str.split('').reduce((stack, char) => {
+    // Если стек уже "сломан" (null), просто прокидываем это состояние дальше
+    if (stack === null) return null;
+
+    const top = stack[stack.length - 1];
+
+    if (openBrackets.has(char)) {
+      // Обработка случая, когда скобки одинаковые (например, '|')
+      if (char === pairs[char] && top === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (stack.length === 0 || pairs[stack.pop()] !== char) {
+      return null;
+    }
+
+    return stack;
+  }, []);
+
+  return resultStack !== null && resultStack.length === 0;
 };
